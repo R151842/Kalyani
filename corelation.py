999643e7a92940874b0bae54ad7af5189506685d
R151842 <47188004+R151842@users.noreply.github.com>
2019-04-09 19:04:03 +0530

    Add files via upload

--- corelation.py
+++ corelation.py
@@ -1,1 +1,13 @@
-import matplotlib.pyplot as pltimport numpy as npx=input('enter seq1 x')h=input('enter seq2 h')n=len(x)h1=np.zeros(len(h))i=0while(i<n):	h1[i]=h[n-1]	i=i+1	n=n-1	print(h1[i])#y=np.convolve(x,h1)#print(y)		
+import  numpy as np
+x=input("enter x values")
+h=input("enter h values")
+h1=np.zeros(len(h))
+n=len(h1)
+i=0
+while(i<len(h1)):
+	h1[i]=h[n-1]
+	print(h1[i])
+	i=i+1
+	n=n-1
+y=np.convolve(x,h1)
+print y
